948f41366d3399a56c7021696f95017405df5610
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-09-13 10:57:39 +0530

    Add files via upload

--- exponentiation.cpp
+++ exponentiation.cpp
@@ -1,26 +1,49 @@
 #include<iostream>
 #include<cmath>
+#include<bits/stdc++.h>
+
+int power(int x, int y, int p);
 
 using namespace std;
 
 int main()
 {
-   unsigned long long  int ncal,i;
+    long long int n,i;
 
-    cin>>ncal;
+    cin>>n;
 
-    for(i=1; i<=ncal; i++)
+    for(i=1; i<=n; i++)
     {
-        long long int a , b , knst, fin=1 ;
-
+        long long int a,b,k,j,fin;
         cin>> a >> b;
-       knst = 1000000007;
 
-       while(b>0)
-       {
-           fin  = (fin * a) % knst;
-           b = b-1;
-       }
+        k=1000000007;
+
+        j = b % k;
+       fin =  power(a,b,k);
+
        cout<< fin <<"\n";
+
     }
+}
+int power(int x, int y, int p)
+{
+    int res = 1;     // Initialize result
+
+    x = x % p; // Update x if it is more than or
+                // equal to p
+
+    if (x == 0) return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
 }
