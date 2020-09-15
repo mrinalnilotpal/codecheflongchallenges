a29bf78eee99eff5c2103f5e08904667f9e7badc
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-09-15 13:32:19 +0530

    Add files via upload

---
+++ learn.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include<cmath>
+
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+//
+//    string s;
+//
+//    getline(cin, s);
+//
+//    cout<< s << "\n";
+//
+
+//int x;
+//
+//while(cin>>x)
+//{
+//    x=x*2;
+//    cout<<x<<"\n";
+//}
+//
+//
+//ll a,b;
+//
+//cin>>a >>b;
+//
+//cout<<a*b;
+
+//  vector<int>  k;
+//  for(int i=1; i<=5; i++)
+//  {
+//      k.push_back(i);
+//  }
+//    for(auto i=k.rbegin(); i!=k.rend(); ++i)
+//  {
+//      cout<< *i <<" ";
+//  }
+
+
+
+// vector<int> g1;
+//
+//    for (int i = 1; i <= 5; i++)
+//        g1.push_back(i);
+//
+//    cout << "Size : " << g1.size();
+//    cout << "\nCapacity : " << g1.capacity();
+//    cout << "\nMax_Size : " << g1.max_size();
+//
+//    // resizes the vector size to 4
+//    g1.resize(4);
+//
+//    // prints the vector size after resize()
+//    cout << "\nSize : " << g1.size();
+//
+//    // checks if the vector is empty or not
+//    if (g1.empty() == false)
+//        cout << "\nVector is not empty";
+//    else
+//        cout << "\nVector is empty";
+//
+//    // Shrinks the vector
+//    g1.shrink_to_fit();
+//    cout << "\nVector elements are: ";
+//    for (auto it = g1.begin(); it != g1.end(); it++)
+////        cout << *it << " ";
+//
+//    return 0;
+
+
+
+
+
+int arr[ ] = {2,3,3,2,1,4,6,68,3,2,2,5,6};
+
+cout<<min(arr,arr+13);
+cout<<max(arr);
+}
