4baeaa2b54ee368df95f42c7d3a23808cdbfa0c3
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-09-18 09:55:52 +0530

    Add files via upload

--- rangesum1.cpp
+++ rangesum1.cpp
@@ -1,34 +1,45 @@
+#include<iostream>
 #include<bits/stdc++.h>
-#include<algorithm>
 
 #define lli long long int
+
 using namespace std;
 
 int main()
 {
-    ios:: sync_with_stdio(0);
     cin.tie(NULL);
 
-    lli n,q,i,a,b,sum,j;
+    lli n,q,i,sum=0,j,a,b;
 
     cin>>n>>q;
 
-     lli arr[n];
+    lli arr[n+1];
 
-     for(i=1; i<=n; i++)
-     {
-         cin>> arr[i];
-     }
+    arr[0]==0;
+    for(i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=1; i<=n; i++)
+    {
+        sum = arr[i] + sum;
+        arr[i] = sum;
 
+    }
+//    for(i=1; i<=n; i++)
+//    {
+//        cout<<arr[i]<<" ";
+//    }
 
-     for(j=1; j<=q; j++)
+    for(j=1; j<=q; j++)
+    {
+        cin>>a>>b;
+        if(a==1)
         {
-            cin>>a>>b;
-            sum=0;
-         for(i=a; i<=b; i++)
-          {
-          sum+= arr[i];
-          }
-     cout<<sum<<"\n";
-     }
+            cout<< arr[b-a+1] - arr[a-1]<<"\n";
+        }
+        else{
+        cout<< arr[b] - arr[a-1]<<"\n";
+    }
+    }
 }
